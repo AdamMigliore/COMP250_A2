11e44b86524059f5c53b9ac17096f2481631b855
AdamDiRe <adam.dire@gmail.com>
2020-02-15 15:49:36 -0500

    pre tester

--- src/TrainLine.java
+++ src/TrainLine.java
@@ -97,26 +97,24 @@ public class TrainLine {
 	public String getName() {
 		return this.lineName;
 	}
-	
-	//YOUR CODE GOES HERE
+
+	// YOUR CODE GOES HERE
 	public int getSize() {
 
 		// YOUR CODE GOES HERE
-		
-		//Iterated through the stations until a null and counts the number of stations
-		
+
+		// Iterated through the stations until a null and counts the number of stations
+
 		/*
-		//If the line is empty we return 0
-		if(leftTerminus==null && rightTerminus==null) {
-			return 0;
-		}
-		*/
-		
+		 * //If the line is empty we return 0 if(leftTerminus==null &&
+		 * rightTerminus==null) { return 0; }
+		 */
+
 		TrainStation tempStation = leftTerminus;
 		int length = 1;
-		while(tempStation.getRight()!=null) {
+		while (tempStation.getRight() != null) {
 			length++;
-			tempStation=tempStation.getRight();
+			tempStation = tempStation.getRight();
 		}
 		return length; // change this!
 	}
@@ -125,155 +123,153 @@ public class TrainLine {
 		this.goingRight = !this.goingRight;
 	}
 
-	//PROBABLY NEED TO CHECK IF THE IMPLEMENTATION IS CORRECT
-	// You can modify the header to this method to handle an exception. You cannot make any other change to the header.
+	// PROBABLY NEED TO CHECK IF THE IMPLEMENTATION IS CORRECT
+	// You can modify the header to this method to handle an exception. You cannot
+	// make any other change to the header.
 	public TrainStation travelOneStation(TrainStation current, TrainStation previous) {
 
 		// YOUR CODE GOES HERE
-		
-		//handle error
+
+		// handle error
 		findStation(current.getName());
-		
-		//if the station has a connection then
+
+		// if the station has a connection then
 		/*
-		 * transfer to it
-		 * if it was our previous then transfer to next station
+		 * transfer to it if it was our previous then transfer to next station
 		 */
-		
+
 		if (current.hasConnection && !current.getTransferStation().equals(previous)) {
-			
+
 			return current.getTransferStation();
-			
-		}else {
+		} else {
 			return getNext(current);
 		}
+
 	}
 
-	//NOT SURE IF PROPERLY IMPLEMENTEDs
-	// You can modify the header to this method to handle an exception. You cannot make any other change to the header.
+	// NOT SURE IF PROPERLY IMPLEMENTEDs
+	// You can modify the header to this method to handle an exception. You cannot
+	// make any other change to the header.
 	public TrainStation getNext(TrainStation station) {
-		
-		//if there is an error it'll throw it
+
+		// if there is an error it'll throw it
 		findStation(station.getName());
-		
+
 		// YOUR CODE GOES HERE
-		if(goingRight) {
-			
-			if(station.isRightTerminal()) {
+		if (goingRight) {
+
+			if (station.isRightTerminal()) {
 				reverseDirection();
 				return station.getLeft();
 			}
-			
+
 			return station.getRight();
-		}else {
-			
-			if(station.isLeftTerminal()) {
+		} else {
+
+			if (station.isLeftTerminal()) {
 				reverseDirection();
 				return station.getRight();
 			}
-			
+
 			return station.getLeft();
 		}
 	}
 
-	// You can modify the header to this method to handle an exception. You cannot make any other change to the header.
+	// You can modify the header to this method to handle an exception. You cannot
+	// make any other change to the header.
 	public TrainStation findStation(String name) {
 
 		// YOUR CODE GOES HERE
-		
-		//might be better to iterate over the array for time efficiency
+
+		// might be better to iterate over the array for time efficiency
 		/*
-		TrainStation tempStation = leftTerminus;
-	
-		while(tempStation.getRight()!=null) {
-			if(tempStation.getName().equals(name)) {
-				return tempStation;
-			}else {
-				tempStation=tempStation.getRight();
-			}
-		}
-		*/
-		
-		for(int i=0;i<lineMap.length;i++) {
-			if(lineMap[i].getName().equals(name)) {
+		 * TrainStation tempStation = leftTerminus;
+		 * 
+		 * while(tempStation.getRight()!=null) { if(tempStation.getName().equals(name))
+		 * { return tempStation; }else { tempStation=tempStation.getRight(); } }
+		 */
+
+		for (int i = 0; i < lineMap.length; i++) {
+			if (lineMap[i].getName().equals(name)) {
 				return lineMap[i];
 			}
 		}
-		throw new StationNotFoundException(name ); // change this!
+		throw new StationNotFoundException(name); // change this!
 	}
-	
-	//NEED MORE EFFICIENT CODE -- THE SWAP SHOULD BE ON THE STATIONS DIRECTLY
-	//YOUR CODE GOES HERE
+
+	// NEED MORE EFFICIENT CODE -- THE SWAP SHOULD BE ON THE STATIONS DIRECTLY
+	// YOUR CODE GOES HERE
 	public void sortLine() {
 		// YOUR CODE GOES HERE
 		boolean repeat = true;
 		int lastIndex = 0;
 		TrainStation tempStation = null;
-		
-		while(repeat) {
-			repeat=false;
-			for(int i=0; i < lineMap.length - 1 - lastIndex;i++) {
-				
-				if(lineMap[i].getName().compareTo(lineMap[i+1].getName()) > 0) {
-					
-					
-					tempStation=lineMap[i];
-					lineMap[i]=lineMap[i+1];
-					lineMap[i+1]=tempStation;
-					
-					//System.out.println("we switched " + lineMap[i+1].getName() + " with " + lineMap[i].getName() );
+
+		while (repeat) {
+			repeat = false;
+			for (int i = 0; i < lineMap.length - 1 - lastIndex; i++) {
+
+				if (lineMap[i].getName().compareTo(lineMap[i + 1].getName()) > 0) {
+
+					tempStation = lineMap[i];
+					lineMap[i] = lineMap[i + 1];
+					lineMap[i + 1] = tempStation;
+
+					// System.out.println("we switched " + lineMap[i+1].getName() + " with " +
+					// lineMap[i].getName() );
 					repeat = true;
 				}
-				
-				
+
 			}
 			lastIndex++;
-			
+
 		}
-		
-		leftTerminus=lineMap[0];
-		rightTerminus=lineMap[lineMap.length-1];
-		
-		//the first terminal is now the terminus left
-		//the last terminal is now the terminus right
+
+		leftTerminus = lineMap[0];
+		rightTerminus = lineMap[lineMap.length - 1];
+
+		// the first terminal is now the terminus left
+		// the last terminal is now the terminus right
+		lineMap[0].setNonTerminal();
 		lineMap[0].setLeftTerminal();
-		lineMap[lineMap.length-1].setRightTerminal();
-		
-		//set initial stations for the first
+		lineMap[0].setLeftTerminal();
+		lineMap[lineMap.length - 1].setRightTerminal();
+
+		// set initial stations for the first
 		lineMap[0].setLeft(null);
 		lineMap[0].setRight(lineMap[1]);
-		
-		//set stations for the last station
-		lineMap[lineMap.length-1].setRight(null);
-		lineMap[lineMap.length-1].setLeft(lineMap[lineMap.length-2]);
-		
-		for (int i=1;i<lineMap.length-1;i++) {
-			lineMap[i].setLeft(lineMap[i-1]);
-			lineMap[i].setRight(lineMap[i+1]);
+
+		// set stations for the last station
+		lineMap[lineMap.length - 1].setRight(null);
+		lineMap[lineMap.length - 1].setLeft(lineMap[lineMap.length - 2]);
+
+		for (int i = 1; i < lineMap.length - 1; i++) {
+			lineMap[i].setNonTerminal();
+			lineMap[i].setLeft(lineMap[i - 1]);
+			lineMap[i].setRight(lineMap[i + 1]);
 		}
 
 	}
-	
-	//YOUR CODE GOES HERE
+
+	// YOUR CODE GOES HERE
 	public TrainStation[] getLineArray() {
 		// YOUR CODE GOES HERE
-		
+
 		/*
-		//if there are no elements return null
-		if (leftTerminus==null && rightTerminus==null) {
-			return null;
-		}
-		*/
-		
-		//might not be good because the reference might not exist after the method is done
+		 * //if there are no elements return null if (leftTerminus==null &&
+		 * rightTerminus==null) { return null; }
+		 */
+
+		// might not be good because the reference might not exist after the method is
+		// done
 		TrainStation[] myLine = new TrainStation[this.getSize()];
-		//we go from the left terminus and return to the last terminus on the right
+		// we go from the left terminus and return to the last terminus on the right
 		TrainStation tempStation = leftTerminus;
-		//our first index
-		myLine[0]=leftTerminus;
-		for(int i = 1; i< myLine.length;i++) {
-			myLine[i]=tempStation.getRight();
-			tempStation=tempStation.getRight();
+		// our first index
+		myLine[0] = leftTerminus;
+		for (int i = 1; i < myLine.length; i++) {
+			myLine[i] = tempStation.getRight();
+			tempStation = tempStation.getRight();
 		}
 		return myLine; // change this
 	}
@@ -290,34 +286,39 @@ public class TrainLine {
 		this.lineMap = array;
 		return array;
 	}
-	
-	//MAYBE THERE IS A MORE EFFICIENT WAY OF DOING THIS
-	//YOUR CODE GOES HERE
+
+	// MAYBE THERE IS A MORE EFFICIENT WAY OF DOING THIS
+	// YOUR CODE GOES HERE
 	public void shuffleLine() {
 
 		// you are given a shuffled array of trainStations to start with
 		TrainStation[] lineArray = this.getLineArray();
 		TrainStation[] shuffledArray = shuffleArray(lineArray);
-		
-		leftTerminus=shuffledArray[0];
-		rightTerminus=shuffledArray[shuffledArray.length-1];
-		
-		//the first terminal is now the terminus left
-		//the last terminal is now the terminus right
+
+		leftTerminus = shuffledArray[0];
+		rightTerminus = shuffledArray[shuffledArray.length - 1];
+
+		// the first terminal is now the terminus left
+		// the last terminal is now the terminus right
+		shuffledArray[0].setNonTerminal();
 		shuffledArray[0].setLeftTerminal();
-		shuffledArray[shuffledArray.length-1].setRightTerminal();
+		shuffledArray[shuffledArray.length - 1].setNonTerminal();
+		shuffledArray[shuffledArray.length - 1].setRightTerminal();
 		
-		//set initial stations for the first
+
+		// set initial stations for the first
 		shuffledArray[0].setLeft(null);
 		shuffledArray[0].setRight(shuffledArray[1]);
-		
-		//set stations for the last station
-		shuffledArray[shuffledArray.length-1].setRight(null);
-		shuffledArray[shuffledArray.length-1].setLeft(shuffledArray[shuffledArray.length-2]);
-		
-		for (int i=1;i<shuffledArray.length-1;i++) {
-			shuffledArray[i].setLeft(shuffledArray[i-1]);
-			shuffledArray[i].setRight(shuffledArray[i+1]);
+
+		// set stations for the last station
+		shuffledArray[shuffledArray.length - 1].setRight(null);
+		shuffledArray[shuffledArray.length - 1].setLeft(shuffledArray[shuffledArray.length - 2]);
+
+		for (int i = 1; i < shuffledArray.length - 1; i++) {
+			shuffledArray[i].setNonTerminal();
+			
+			shuffledArray[i].setLeft(shuffledArray[i - 1]);
+			shuffledArray[i].setRight(shuffledArray[i + 1]);
 		}
 
 		// YOUR CODE GOES HERE
--- src/TrainNetwork.java
+++ src/TrainNetwork.java
@@ -2,100 +2,130 @@ public class TrainNetwork {
 	final int swapFreq = 2;
 	TrainLine[] networkLines;
 
-    public TrainNetwork(int nLines) {
-    	this.networkLines = new TrainLine[nLines];
-    }
-    
-    public void addLines(TrainLine[] lines) {
-    	this.networkLines = lines;
-    }
-    
-    public TrainLine[] getLines() {
-    	return this.networkLines;
-    }
-    
-  //YOUR CODE GOES HERE
-    public void dance() {
-    	System.out.println("The tracks are moving!");
-    	//YOUR CODE GOES HERE
-    	for (int i = 0; i < networkLines.length; i++) {
-    		networkLines[i].shuffleLine();
-    	}
-    }
-    
-  //YOUR CODE GOES HERE
-    public void undance() {
-    	//YOUR CODE GOES HERE
-    	for (int i = 0; i < networkLines.length; i++) {
-    		networkLines[i].sortLine();
-    	}
-    }
-    
-    //YOUR CODE GOES HERE
-    public int travel(String startStation, String startLine, String endStation, String endLine) {
-    	
-    	TrainLine curLine = null; //use this variable to store the current line.
-    	TrainStation curStation= null; //use this variable to store the current station. 
-    	
-    	
-    	int hoursCount = 0;
-    	System.out.println("Departing from "+startStation);
-    	
-    	//YOUR CODE GOES HERE
-    	
-    	
-    	while(true /*you can change this*/) {
-    		
-    		if(hoursCount == 168) {
-    			System.out.println("Jumped off after spending a full week on the train. Might as well walk.");
-    			return hoursCount;
-    		}
-    		
-    		//prints an update on your current location in the network.
-	    	System.out.println("Traveling on line "+curLine.getName()+":"+curLine.toString());
-	    	System.out.println("Hour "+hoursCount+". Current station: "+curStation.getName()+" on line "+curLine.getName());
-	    	System.out.println("=============================================");
-	    	
-	    	break; //remove this! this break is only there so the template compiles.
-    		}
-	    	
-	    	System.out.println("Arrived at destination after "+hoursCount+" hours!");
-	    	return hoursCount;
-    }
-    
-    
-    //you can extend the method header if needed to include an exception. You cannot make any other change to the header.
-    public TrainLine getLineByName(String lineName){
-    	//YOUR CODE GOES HERE
-    	for (int i = 0; i < networkLines.length; i++) {
-    		if (networkLines[i].getName().equals(lineName)) {
-    			return networkLines[i];
-    		}
-    	}
-    	
-    	throw new LineNotFoundException(lineName); //change this
-    }
-    
-  //prints a plan of the network for you.
-    public void printPlan() {
-    	System.out.println("CURRENT TRAIN NETWORK PLAN");
-    	System.out.println("----------------------------");
-    	for(int i=0;i<this.networkLines.length;i++) {
-    		System.out.println(this.networkLines[i].getName()+":"+this.networkLines[i].toString());
-    		}
-    	System.out.println("----------------------------");
-    }
+	public TrainNetwork(int nLines) {
+		this.networkLines = new TrainLine[nLines];
+	}
+
+	public void addLines(TrainLine[] lines) {
+		this.networkLines = lines;
+	}
+
+	public TrainLine[] getLines() {
+		return this.networkLines;
+	}
+
+	// YOUR CODE GOES HERE
+	public void dance() {
+		System.out.println("The tracks are moving!");
+		// YOUR CODE GOES HERE
+		for (int i = 0; i < networkLines.length; i++) {
+			networkLines[i].shuffleLine();
+		}
+	}
+
+	// YOUR CODE GOES HERE
+	public void undance() {
+		// YOUR CODE GOES HERE
+		for (int i = 0; i < networkLines.length; i++) {
+			networkLines[i].sortLine();
+		}
+	}
+
+	// YOUR CODE GOES HERE
+	public int travel(String startStation, String startLine, String endStation, String endLine) {
+
+		int hoursCount = 0;
+		TrainLine curLine = null; // use this variable to store the current line.
+		TrainStation curStation = null; // use this variable to store the current station.
+
+		try {
+			curLine = getLineByName(startLine); // use this variable to store the current line.
+			curStation = curLine.findStation(startStation); // use this variable to store the current station.
+		} catch (StationNotFoundException snfe) {
+			return 168;
+		}
+
+		// idk if i can add variables
+		TrainStation prevStation = null;
+		TrainStation tempStation = null;
+
+		System.out.println("Departing from " + startStation);
+
+		// YOUR CODE GOES HERE
+
+		// condition should be if you arrived
+		while (!curStation.getName().equals(endStation)
+				&& !curLine.getName().equals(endLine) /* you can change this */) {
+
+			// break
+			if (hoursCount == 168) {
+				System.out.println("Jumped off after spending a full week on the train. Might as well walk.");
+				return hoursCount;
+			}
+			
+			if(hoursCount%2==0 && hoursCount!=0) {
+				dance();
+			}
+
+				// changes station
+				tempStation = curStation;
+				// when we get a line switch it doesnt work
+				//returning null
+				curStation = curLine.travelOneStation(curStation, prevStation);
+				curLine = curStation.getLine();
+				prevStation = tempStation;
+
+			// increments hours
+			hoursCount++;
+
+			// prints an update on your current location in the network.
+			System.out.println("Traveling on line " + curLine.getName() + ":" + curLine.toString());
+			System.out.println("Hour " + hoursCount + ". Current station: " + curStation.getName() + " on line "
+					+ curLine.getName());
+			System.out.println("=============================================");
+
+			// break; //remove this! this break is only there so the template compiles.
+			// make the tracks move
+
+		}
+
+		System.out.println("Arrived at destination after " + hoursCount + " hours!");
+		return hoursCount;
+	}
+
+	// you can extend the method header if needed to include an exception. You
+	// cannot make any other change to the header.
+	public TrainLine getLineByName(String lineName) {
+		// YOUR CODE GOES HERE
+		for (int i = 0; i < networkLines.length; i++) {
+			if (networkLines[i].getName().equals(lineName)) {
+				return networkLines[i];
+			}
+		}
+
+		throw new LineNotFoundException(lineName); // change this
+	}
+
+	// prints a plan of the network for you.
+	public void printPlan() {
+		System.out.println("CURRENT TRAIN NETWORK PLAN");
+		System.out.println("----------------------------");
+		for (int i = 0; i < this.networkLines.length; i++) {
+			System.out.println(this.networkLines[i].getName() + ":" + this.networkLines[i].toString());
+		}
+		System.out.println("----------------------------");
+	}
 }
 
 //exception when searching a network for a LineName and not finding any matching Line object.
 class LineNotFoundException extends RuntimeException {
-	   String name;
+	String name;
 
-	   public LineNotFoundException(String n) {
-	      name = n;
-	   }
-
-	   public String toString() {
-	      return "LineNotFoundException[" + name + "]";
-	   }
+	public LineNotFoundException(String n) {
+		name = n;
 	}
+
+	public String toString() {
+		return "LineNotFoundException[" + name + "]";
+	}
+}
